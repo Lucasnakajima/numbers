a5bc7bd134f5bccffa35b7f1a6c78f1d1ce5c0bb
Lucas Nakajima <nakajima.lucas02@gmail.com>
2022-10-22 10:20:57 +0100

    Done step 9

--- src/main/java/com/aor/numbers/PositiveFilter.java
+++ src/main/java/com/aor/numbers/PositiveFilter.java
@@ -3,8 +3,6 @@ package com.aor.numbers;
 public class PositiveFilter implements GenericListFilter{
     @Override
     public boolean accept(Integer number) {
-        if(number>0)
-            return true;
-        return false;
+        return number>0;
     }
 }
